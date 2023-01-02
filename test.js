a8db5b48951161b067c9fb6e54b581f3301390fc
Illya Myhalyk <ilamihalik@gmail.com>
2023-01-02 14:15:24 +0200

    delete test files

--- test.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-   $("span").mouseover(function (){
-    $(this).css("weight")
-   })
-      })
--- testTwo.js
+++
@@ -1,4 +0,0 @@
-
-
-import {temp1, temp2 } from './test work/test.js';
-console.log(temp1);
